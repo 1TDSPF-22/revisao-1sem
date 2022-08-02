0332cd9bed5877f63091638ef21a2ecd33db8ccb
andteixeira10 <rm93032@fiap.com.br>
2022-08-02 09:42:08 -0300

    Variaveis explicadas

---
+++ rm93032/js/script.js
@@ -0,0 +1,9 @@
+//Variáveis 
+//var - Variável de escopo global
+var nome = "João"
+
+//let- Variavel de escopo local
+let sobrenome = "João"
+
+//Constante - Variavel que não pode ser alterada
+const idade = 20
