70839772ea368ac2349e0bcd12bcd0fa59af0a34
ErickSoaresM <rm94764@fiap.com.br>
2022-08-02 08:43:17 -0300

    console.log/variaveis/escopo/hoisting

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-console.log("Olá Mundo!")
+//console.log("Olá Mundo!")
 
+
+//HOISTING - Ocorre quando uma variável é declarada e atribuída antes de ser utilizada
+nome = "Geldo"
+//console.log(nome)
+
+sobrenome = "vagabundo nato"
+console.log (nome + sobrenome)
+
+//Variáveis
+
+//var - variavel de escopo global
+//Escopo global - Tudo que está fora do bloco
+//Exemplo de escopo global - window
+var nome = "Erick"
+
+//let - Variável de escopo local
+//Escopo local - tudo que está dentro do bloco 
+//Exemplo de escopo local - if, for, while
+let sobrenome = "Soares"
+
+//Constante - Não pode ser alterada 
+const idade = 18
