936b8057c1d601ebd9520bd4fc2c0f4b6834e85e
augustopdro <rm94990@fiap.com.br>
2022-08-02 08:43:26 -0300

    Console.log/variáveis/escopo

---
+++ rm94990/js/script.js
@@ -0,0 +1,15 @@
+//console.log() - Imprime na tela
+//console.log("Olá mundo!")
+
+//document.write - Escreve na tela
+//document.write("Olá mundo!")
+
+//Variáveis
+//Var - Variável de escopo global
+var nome = "João";
+
+//let - variável de escopo local 
+let sobrenome = "Silva";
+
+//Const - Não pode ser alterada
+const idade = 19;
