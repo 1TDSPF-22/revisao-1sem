a68bdfe3049564c683ac4c2db7f494c54a5ec9d3
btwbeatriz <rm95246@fiap.com.br>
2022-08-02 08:43:47 -0300

    console.log hoisting

---
+++ rm95246/script.js
@@ -0,0 +1,33 @@
+//Comentário de uma linha
+
+/**
+ * Comentário de múltiplas linhas
+ */
+
+//console.log() - Imprime no console
+//console.log("Olá Mundo!")
+
+//document.write() - Imprime na tela
+//document.write("Olá Mundo!")
+
+//HOISTING - Ocorre quando uma variável é declarada e atribuída antes de ser utilizada
+//nome = "Biscoito"
+//console.log(nome)
+
+//sobrenome = "de Polvilho"
+//console.log(nome + " " + sobrenome)
+
+//Variáveis
+
+//Variável de escopo global
+//Escopo global - Tudo que está fora do bloco
+//Exemplo de escopo global - window
+//var nome = "João"
+
+//let - variável de escopo local
+//Escopo local - Tudo que está dentro do bloco
+//Exemplo de escopo local - if, for, while
+//let sobrenome = "Silva"
+
+//Constante - Não pode ser alterada
+//const idade = 20
