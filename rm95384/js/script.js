388190b8d4389f0a00c4937f99127a5f448a7c7f
LLK26 <RM95384@fiap.com.br>
2022-08-02 08:43:29 -0300

    Console.log / variaveis / escopo / hosting

---
+++ rm95384/js/script.js
@@ -0,0 +1,32 @@
+
+//Comentário de uma linha
+/** Comentário de múltiplas linhas
+ */
+
+//Console.log() - Imprime na tela
+//console.log("Olá Mundo!")
+
+//document.write() - Imprime na tela
+//document.write("Olá Mundo")
+
+//HOISTING - Ocorre quando uma variável é declarada e atribuída antes de ser utilizada
+nome = "Igor"
+//console.log(nome)
+
+sobrenome = "Mão de Pedreiro"
+console.log(nome + " " + sobrenome)
+
+//Variáveis
+
+//var - Variável de escopo global
+//Escopo global - Tudo que está fora do bloco
+//Exemplo de escopo global - window
+var nome = "João"
+
+//let - Variável de escopo local
+//Escopo local - Tudo que está dentro do bloco
+//Exemplo de escopo local - if, for, while
+let sobrenome = "Silva"
+
+//Constante - Não pode ser alterada
+const idade = 20
