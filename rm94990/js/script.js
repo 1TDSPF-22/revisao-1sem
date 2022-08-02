7c0c7f2297f3b12b3633d508c74ce4f6dc4bb368
augustopdro <rm94990@fiap.com.br>
2022-08-02 09:13:27 -0300

    Objetos.

--- rm94990/js/script.js
+++ rm94990/js/script.js
@@ -6,10 +6,52 @@
 
 //Variáveis
 //Var - Variável de escopo global
-var nome = "João";
+//var nome = "João";
 
 //let - variável de escopo local 
-let sobrenome = "Silva";
+//let sobrenome = "Silva";
 
 //Const - Não pode ser alterada
-const idade = 19;
+//const idade = 19;
+
+//Objeto - Coleção de chaves e valores
+//Exemplo
+
+//const pessoa = {
+//    nome: "João",
+//    sobrenome: "Silva",
+//    idade: 20,
+//    trabalhando: false,
+//}
+
+//pessoa.nome = "Pedro"
+//pessoa.sobrenome = "Sigma"
+//pessoa.idade = 19
+//pessoa.trabalhando = true
+//console.log(pessoa)
+
+
+//Gravando os dados do forms no objeto
+const pessoa = {
+    nmTask: "",
+    dtTask: "",
+    descTask: ""
+}
+
+//Pegando os dados do Formulário
+function pegarDados(){
+    pessoa.nmTask = document.getElementById("nmTaskId").value
+    pessoa.dtTask = document.getElementById("dtTaskId").value
+    pessoa.descTask = document.getElementById("descTaskId").value
+
+    exibirDados()
+}
+
+//Exibindo os dados do objeto
+function exibirDados(){
+    console.log(pessoa)
+}
+
+//Recuperando o botão
+const btn = document.getElementById("btnEnviar")
+btn.addEventListener("click", pegarDados)
