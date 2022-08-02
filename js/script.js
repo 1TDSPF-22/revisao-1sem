c5b96b644fe5a97cb94efd77facb8643d9a27bc4
gabtreze <RM95603@fiap.com>
2022-08-02 08:44:07 -0300

    Aprendendo Variaveis/ Escopo/ Hoisting

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Console.log() - imprime na tela
+// console.log("Olá Mundo!")
+
+//document.write() - Imprime na tela
+//document.write("Olá Mundo!")
+
+
+
+
+
+//Variaveis
+
+
+// Escopo global - tudo que esta fora do bloco
+// Exemplo de escopo global - window
+//var nome = "João"
+
+//let - Variavel de escopo local
+// Escopo local - tudo que esta dentro do bloco
+// Exemplo de escopo local - if, for, while
+//let sobrenome = "Silva"
+
+//Constante - não pode ser alterada
+//const idade = 20
