c813b9ad4805f08200d6ea9e6c929b97e3b568dc
andteixeira10 <rm93032@fiap.com.br>
2022-08-02 10:22:16 -0300

    Objeto e botao adicionado

--- rm93032/js/script.js
+++ rm93032/js/script.js
@@ -1,9 +1,58 @@
 //Variáveis 
 //var - Variável de escopo global
-var nome = "João"
+//var nome = "João"
 
 //let- Variavel de escopo local
-let sobrenome = "João"
+//let sobrenome = "João"
 
 //Constante - Variavel que não pode ser alterada
-const idade = 20
+//const idade = 20
+
+
+//Objeto - Coleção de chaves e valores 
+//Exemplo de objeto
+//const pessoa = {
+    //nome: "Joao",
+    //sobrenome: "Silva",
+   // idade: 20,
+    //vendendo: true
+//}
+
+//pessoa.nome = "Igor"
+//pessoa.sobrenome = "Mão de Pedreiro"
+//pessoa.idade = 30
+//pessoa.vendendo =false
+//console.log(pessoa)
+
+//pessoa.idade = 40
+//console.log(pessoa)
+
+
+//JSON - JavaScript Object Notation
+
+
+//Gravando os dados do Formulário no Objeto
+const pessoa = {
+    nmTask: "",
+    dtTask: "",
+    descTask: ""
+
+}
+
+//Pegando os dados do Formulário
+//Aqui estamos pegando os dados do formulário e salvando no objeto
+//Arrow function - Função anônima
+//const pegarDados = () => {
+  //  pessoa.nmTask = document.getElementById("nmTaskId").value
+    //pessoa.dtTask = document.getElementById("dtTaskId").value
+    //pessoa.descTask = document.getElementById("descTaskId").value
+//}
+
+//Exibindo os dados do Objeto
+function exibirDados(){
+   console.log(pessoa)
+}
+
+//Recuperando o botão
+const btn = document.getElementById("btnEnviar")
+btn.addEventListener("click", pegarDados)
